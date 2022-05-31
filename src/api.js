b5ba8b0245e5116d2d2850c8049d828786285913
ManojPatidar <manojpatidar9689@gmail.com>
2022-05-31 18:50:55 +0530

    changes in api.js file

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://15.207.21.72:3211/shethink/v1",
+  baseURL: "http://3.91.205.100:3211/shethink/v1",
   headers: { Accept: "application/json" },
 });
